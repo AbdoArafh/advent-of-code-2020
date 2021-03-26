6525586ae9f66c125976de7730ae1be759a43e37
username <abdoarafh54@gmail.com>
2021-03-26 14:36:56 +0200

    part two should be down but I couldn't run the program bc it would halv taking ages so I'm gonna be reading others' code to have a better aproach just to know it

--- advent of code day 17/solution2.py
+++ advent of code day 17/solution2.py
@@ -1,9 +1,9 @@
 import math
 n = 50
-grid = [[[0 for k in range(n)] for j in range(n)] for i in range(n)]
+grid = [[[[0 for k in range(n)] for j in range(n)] for i in range(n)] for w in range(n)]
 
 def loadStart():
-    with open("input.txt", 'r') as file:
+    with open("test.txt", 'r') as file:
         cells = file.read().split()
     cols = len(cells)
     rows = len(cells[0])
@@ -12,30 +12,33 @@ def loadStart():
             x = math.floor(n/2 - (cols/2) + i)
             y = math.floor(n/2 - (rows/2) + j)
             z = math.floor(n/2)
+            w = math.floor(n/2)
             if cells[i][j] == '#':
-                grid[x][y][z] = 1
+                grid[x][y][z][w] = 1
 
 
-def countNeighbors(x, y, z):
+def countNeighbors(x, y, z, w):
     neighbors = 0
     for i in range(-1, 2):
         for j in range(-1, 2):
             for k in range(-1, 2):
-                try:
-                    neighbors += grid[x + i][y + j][z + k]
-                except IndexError:
-                    neighbors += 0
-    return neighbors - grid[x][y][z]
+                for l in range(-1, 2):
+                    try:
+                        neighbors += grid[x + i][y + j][z + k][w + l]
+                    except IndexError:
+                        neighbors += 0
+    return neighbors - grid[x][y][z][w]
 
 
 def generate():
     global grid
-    next = [[[0 for k in range(n)] for j in range(n)] for i in range(n)]
+    next = [[[[0 for k in range(n)] for j in range(n)] for i in range(n)] for l in range(n)]
     for x in range(n):
         for y in range(n):
             for z in range(n):
-                neighbors = countNeighbors(x, y, z)
-                next[x][y][z] = fate(neighbors, grid[x][y][z])
+                for w in range(n):
+                    neighbors = countNeighbors(x, y, z, w)
+                    next[x][y][z][w] = fate(neighbors, grid[x][y][z][w])
     grid = next
     return count()
 
@@ -54,7 +57,8 @@ def count():
     for i in range(n):
         for j in range(n):
             for k in range(n):
-                counter += grid[i][j][k]
+                for l in range(n):
+                    counter += grid[i][j][k][l]
     return counter
 
 
