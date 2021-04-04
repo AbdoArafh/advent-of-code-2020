7dcff747c44f9c29f48d6f3dc9f15687c7454a0c
AbdoArafh <abdoarafh54@gmail.com>
2021-04-04 11:45:47 +0200

    solved the test input but not the real one for part 1

--- day 18/solution.py
+++ day 18/solution.py
@@ -1,7 +1,7 @@
 import re
 
 with open('test.txt', 'r') as file:
-    problems = file.read().strip().split("\n")[:4]
+    problems = file.read().strip().split("\n")[4]
 
 # results = []
 # for problem in problems:
@@ -13,42 +13,37 @@ with open('test.txt', 'r') as file:
 
 def solve(problem):
     if '(' in problem:
-        # print(problem[:re.search(r"\(.+\)", problem).start()])
-        # print(re.search(r"\(.+\)", problem).group()[1:-1])
-        # print(problem[re.search(r"\(.+\)", problem).end():])
         # exep = r'(\((\(.+\)*)+\))'
-        exep = r'\((\(.+\).*)+?\)'
+        exep = r'\((.*\(.+\).*)+?\)'
         if bool(re.search(exep, problem)):
             matchObject = re.search(exep, problem)
             start = matchObject.start()
             end = matchObject.end()
             group = matchObject.group()[1:-1]
-            print(f"condition matched in: {problem} and group {group}")
+            # print(f"condition matched in: {problem} and group {group}")
         else:
             start = re.search(r'\(', problem).start()
             end = re.search(r'\)', problem).end()
             group = problem[start+1:end-1]
-            print(f"condition didn't matched in: {problem}")
-        # print(start, end, group)
-        try:
-            return solve(problem[:start] + solve(group) + problem[end:])
-        except AttributeError:
-            print(start, end, group)
-            return problem
+            # print(f"condition didn't matched in: {problem}")
+        print(start, end, group)
+        return solve(problem[:start] + solve(group) + problem[end:])
+        # except AttributeError:
+        #     print(start, end, group)
+            # return problem
 
     exep = re.search(r'\d+ ?\D+? ?\d+', problem)
     if bool(exep):
         loc = {}
         exec("x = " + problem[exep.start():exep.end()], loc)
         x = loc['x']
-        try:
-            return solve(str(x)+ problem[exep.end():])
-        except IndexError:
-            return str(x)
+        return solve(str(x)+ problem[exep.end():])
+        # except IndexError:
+            # return str(x)
     else:
         return problem
         
 # print(sum[int(solve(problem)) for problem in problems])
 # print(sum([int(solve(x)) for x in problems]))
-# print(solve(problems))
-print([solve(x) for x in problems])
+print(solve(problems))
+# print(sum([int(solve(x).strip()) for x in problems]))
